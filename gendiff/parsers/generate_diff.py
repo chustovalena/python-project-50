e1837ab3599fd83208dc8089a4a9da79f8879116
Елена Чустова <elenacustova@MacBook-Air-Elena.local>
2025-03-29 21:15:51 +0300

    create generate_diff.py

---
+++ gendiff/parsers/generate_diff.py
@@ -0,0 +1,9 @@
+import json
+
+
+with open ('parsers/file1.json', 'r') as file:
+	data1 = json.load(file)
+with open ('parsers/file2.json', 'r') as file:
+	data2 = json.load(file)
+
+print(data1, data2)
