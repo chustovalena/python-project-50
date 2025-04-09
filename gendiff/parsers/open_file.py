37e6d3e7225b836898ccd64a79387cf4b864f5e4
Елена Чустова <elenacustova@MacBook-Air-Elena.local>
2025-04-10 00:54:05 +0300

    update open_file.py

--- gendiff/parsers/open_file.py
+++ gendiff/parsers/open_file.py
@@ -1,7 +1,10 @@
 import json
 
 
-with open ('parsers/file1.json', 'r') as file:
-	data_a = json.load(file)
-with open ('parsers/file2.json', 'r') as file:
-	data_b = json.load(file)
+def open_files(file1_path, file2_path):
+	with open(file1_path, 'r') as file:
+		data_a = json.load(file)
+	with open(file2_path, 'r') as file:
+		data_b = json.load(file)
+
+	return data_a, data_b
