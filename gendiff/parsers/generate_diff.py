4bfb9be6fde4786e52289cee0cec5f6ed014f46a
Елена Чустова <elenacustova@MacBook-Air-Elena.local>
2025-04-10 00:26:57 +0300

    create function generate diff

--- gendiff/parsers/generate_diff.py
+++ gendiff/parsers/generate_diff.py
@@ -1,7 +1,18 @@
-import json
-
-
-with open ('parsers/file1.json', 'r') as file:
-	data1 = json.load(file)
-with open ('parsers/file2.json', 'r') as file:
-	data2 = json.load(file)
+def gen_diff(data_a, data_b):
+  data_a = dict(sorted(data_a.items()))
+  data_b = dict(sorted(data_b.items()))
+  diff = []
+  for key in data_a.keys():
+      if key in data_b:
+          if data_a[key] == data_b[key]:
+            diff.append('  ' + key + ' : ' + str(data_a[key]))
+          else:
+            diff.append('- ' + key + ' : ' + str(data_a[key]))
+            diff.append('+ ' + key + ' : ' + str(data_b[key]))
+      else:
+        diff.append('- ' + key + ' : ' + str(data_a[key]))
+  for key in data_b.keys():
+      if key not in data_a:
+        diff.append('+ ' + key + ' : ' + str(data_b[key]))
+  res = '{\n' + '\n'.join(diff) + '\n}'
+  return res
