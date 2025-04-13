531417e2b615b2a0c75a9819229fea184ac23511
Елена Чустова <elenacustova@MacBook-Air-Elena.local>
2025-04-13 19:58:54 +0300

    add test_generate_diff

---
+++ tests/test_generate_diff.py
@@ -0,0 +1,12 @@
+from gendiff.parsers.generate_diff import gen_diff
+
+def empty_data():
+	assert gen_diff({},{}) == '{\n' + '\n}'
+
+
+def no_second_data():
+	assert gen_diff({'hey': 'karen'}, {}) == '{\n' + '- hey : karen' + '\n}'
+
+
+def no_first_data():
+	assert gen_diff({}, {'hey': 'karen'}) == '{\n' + '+ hey : karen' + '\n}'
