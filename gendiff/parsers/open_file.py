915bce5896cb32a105dc988a1d7d1bd332124a2a
Елена Чустова <elenacustova@MacBook-Air-Elena.local>
2025-04-17 17:10:49 +0300

    add yaml in func

--- gendiff/parsers/open_file.py
+++ gendiff/parsers/open_file.py
@@ -1,10 +1,17 @@
 import json
-
+import yaml
+from pathlib import Path
 
 def open_files(file1_path, file2_path):
-	with open(file1_path, 'r') as file:
-		data_a = json.load(file)
-	with open(file2_path, 'r') as file:
-		data_b = json.load(file)
-
+	file_path = Path(file1_path)
+	if file_path.suffix.lower() == '.json':
+		with open(file1_path, 'r') as file:
+			data_a = json.load(file)
+		with open(file2_path, 'r') as file:
+			data_b = json.load(file)
+	elif file_path.suffix.lower() == '.yaml':
+		with open(file1_path, 'r') as file:
+			data_a = yaml.safe_load(file)
+		with open(file2_path, 'r') as file:
+			data_b = yaml.safe_load(file)
 	return data_a, data_b
