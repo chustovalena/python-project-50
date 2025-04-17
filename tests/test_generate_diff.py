4d67e9c7a6d820e5f240c0cc6fd4cd8ac1102bba
Елена Чустова <elenacustova@MacBook-Air-Elena.local>
2025-04-17 22:59:51 +0300

    update test_generate_diff.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -6,14 +6,14 @@ def test_empty_data():
 
 
 def test_no_second_data():
-	assert gen_diff({'hey': 'karen'}, {}) == '{\n' + '- hey : karen' + '\n}'
+	assert gen_diff({'hey': 'karen'}, {}) == '{\n' + '  - hey: karen' + '\n}'
 
 
 def test_no_first_data():
-	assert gen_diff({}, {'hey': 'karen'}) == '{\n' + '+ hey : karen' + '\n}'
+	assert gen_diff({}, {'hey': 'karen'}) == '{\n' + '  + hey: karen' + '\n}'
 
 def same_data():
-	assert gen_diff({'hello': 'world'},{'hello': 'world'}) == '{\n' + '  hey : karen' + '\n}'
+	assert gen_diff({'hello': 'world'},{'hello': 'world'}) == '{\n' + '    hey: karen' + '\n}'
 
 def in_a_and_in_b_but_different():
-	assert gen_diff({'hello': 'world'}, {'hello': 'sam'}) == '{\n' + '- hello : world' + '\n' + '+ hello : sam' + '\n}'
+	assert gen_diff({'hello': 'world'}, {'hello': 'sam'}) == '{\n' + '  - hello: world' + '\n' + '  + hello : sam' + '\n}'
