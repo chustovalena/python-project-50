65d1d6332ffe5540c31e66130c27112026ef6103
Елена Чустова <elenacustova@MacBook-Air-Elena.local>
2025-04-17 17:36:33 +0300

    fix open_file.py

--- gendiff/parsers/open_file.py
+++ gendiff/parsers/open_file.py
@@ -9,7 +9,7 @@ def open_files(file1_path, file2_path):
 			data_a = json.load(file)
 		with open(file2_path, 'r') as file:
 			data_b = json.load(file)
-	elif file_path.suffix.lower() == '.yaml':
+	elif file_path.suffix.lower() in ['.yaml', '.yml']:
 		with open(file1_path, 'r') as file:
 			data_a = yaml.safe_load(file)
 		with open(file2_path, 'r') as file:
