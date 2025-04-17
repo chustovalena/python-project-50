b3d86734279f07fbad5debc11928d5eebad4e79a
Елена Чустова <elenacustova@MacBook-Air-Elena.local>
2025-04-17 20:47:00 +0300

    fix open_file.py

--- gendiff/parsers/open_file.py
+++ gendiff/parsers/open_file.py
@@ -4,14 +4,18 @@ from pathlib import Path
 
 def open_files(file1_path, file2_path):
 	file_path = Path(file1_path)
+	file_path2 = Path(file2_path)
 	if file_path.suffix.lower() == '.json':
 		with open(file1_path, 'r') as file:
 			data_a = json.load(file)
-		with open(file2_path, 'r') as file:
-			data_b = json.load(file)
 	elif file_path.suffix.lower() in ['.yaml', '.yml']:
 		with open(file1_path, 'r') as file:
 			data_a = yaml.safe_load(file)
+	if file_path2.suffix.lower() == '.json':
+		with open(file2_path, 'r') as file:
+			data_b = json.load(file)
+	elif file_path2.suffix.lower() in ['.yaml', '.yml']:
 		with open(file2_path, 'r') as file:
 			data_b = yaml.safe_load(file)
+	
 	return data_a, data_b
