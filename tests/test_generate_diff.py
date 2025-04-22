342ceafb5a4eafa16d53b8557d368c9ccafadf9d
Елена Чустова <elenacustova@MacBook-Air-Elena.local>
2025-04-22 22:05:56 +0300

    try to fix

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,19 +1,19 @@
-from gendiff.parsers.generate_diff import gen_diff
+from gendiff.parsers.generate_diff import generate_diff
 import pytest
 
 def test_empty_data():
-	assert gen_diff({}, {}) == '{\n' + '\n}'
+	assert generate_diff({}, {}) == '{\n' + '\n}'
 
 
 def test_no_second_data():
-	assert gen_diff({'hey': 'karen'}, {}) == '{\n' + '  - hey: karen' + '\n}'
+	assert generate_diff({'hey': 'karen'}, {}) == '{\n' + '  - hey: karen' + '\n}'
 
 
 def test_no_first_data():
-	assert gen_diff({}, {'hey': 'karen'}) == '{\n' + '  + hey: karen' + '\n}'
+	assert generate_diff({}, {'hey': 'karen'}) == '{\n' + '  + hey: karen' + '\n}'
 
 def test_same_data():
-	assert gen_diff({'hello': 'world'},{'hello': 'world'}) == '{\n' + '    hello: world' + '\n}'
+	assert generate_diff({'hello': 'world'},{'hello': 'world'}) == '{\n' + '    hello: world' + '\n}'
 
 def test_in_a_and_in_b_but_different():
-	assert gen_diff({'hello': 'world'}, {'hello': 'sam'}) == '{\n' + '  - hello: world' + '\n' + '  + hello: sam' + '\n}'
+	assert generate_diff({'hello': 'world'}, {'hello': 'sam'}) == '{\n' + '  - hello: world' + '\n' + '  + hello: sam' + '\n}'
