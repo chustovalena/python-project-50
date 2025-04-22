60e210957d7b5a32fb82591a0a391af46d41e8c7
Елена Чустова <elenacustova@MacBook-Air-Elena.local>
2025-04-22 21:56:50 +0300

    create format.py - stylish

---
+++ gendiff/formatters/format.py
@@ -0,0 +1,41 @@
+from gendiff.parsers.generate_diff import gen_diff
+
+
+def formatting(diff, depth=0):
+    lines = []
+    indent = ' ' * (depth * 4)
+    for key, value in diff.items():
+        if value['status'] == 'nested':
+            lines.append(f"{indent}    {key}: {{")
+            lines.extend(formatting(value['children'], depth + 1))
+            lines.append(f"{indent}    }}")
+        elif value['status'] == 'changed':
+            lines.append(f"{indent}  - {key}: {format_value(value['old_value'], depth)}")
+            lines.append(f"{indent}  + {key}: {format_value(value['new_value'], depth)}")
+        elif value['status'] == 'added':
+            lines.append(f"{indent}  + {key}: {format_value(value['value'], depth)}")
+        elif value['status'] == 'removed':
+            lines.append(f"{indent}  - {key}: {format_value(value['value'], depth)}")
+        elif value['status'] == 'unchanged':
+            lines.append(f"{indent}    {key}: {format_value(value['value'], depth)}")
+    return lines
+
+
+
+def format_value(value, depth):
+    indent = ' ' * ((depth + 1) * 4)
+    bracket_indent = ' ' * (depth * 4)
+
+    if isinstance(value, dict):
+        lines = ['{']
+        for key, v in value.items():
+            lines.append(f"{indent}    {key}: {format_value(v, depth + 1)}")
+        lines.append(f"{bracket_indent}    }}")
+        return '\n'.join(lines)
+    else:
+        return str(value)
+
+
+
+def res_formatting(lines):
+    return "{\n" + '\n'.join(lines) + "\n}"
