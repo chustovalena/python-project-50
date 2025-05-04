167ef06d6570ab10345624f8ff92ad8cce89a0d0
Елена Чустова <elenacustova@MacBook-Air-Elena.local>
2025-05-04 19:35:30 +0300

    add plain

--- gendiff/parsers/parser.py
+++ gendiff/parsers/parser.py
@@ -7,8 +7,12 @@ def parser_args():
 	
 	parser.add_argument('first_file')
 	parser.add_argument('second_file')
-	parser.add_argument('-f', '--format', help='set format of output')
-	
+	parser.add_argument(
+        '-f', '--format',
+        choices=['stylish', 'plain'],
+        default='stylish',
+        help='set format of output (stylish, plain)'
+    )
 	args = parser.parse_args()
 
 	return args
